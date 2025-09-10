324b644f66155dee2563d30f000fb9b58e47a937
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2025-09-10 22:16:56 +0530

    Added new method calls

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ if __name__ == "__main__":
     if not storage_ob.get_credentials():
         print("Exiting as authentication failed.")
     else:
-        BUCKET_NAME = 'hello_world_bucket24_saurabh'
-        storage_ob.create_bucket(bucket_name=BUCKET_NAME)
-        print()
-        storage_ob.list_buckets()
+        # BUCKET_NAME = f"{storage_ob.get_project_id()}-test-bucket-example"
+        BUCKET_NAME = "udemy-cource"
+        storage_ob.list_blobs(bucket_name=BUCKET_NAME)
+        # storage_ob.create_blob(bucket_name=BUCKET_NAME, source_file_name='packsges.txt', destination_blob_name="packsges.txt")
+
+        # print(storage_ob.get_bucket(bucket_name=BUCKET_NAME))
+        # storage_ob.create_bucket(bucket_name=BUCKET_NAME)
+        # storage_ob.list_buckets()
+        # print(storage_ob.get_bucket(bucket_name=BUCKET_NAME))

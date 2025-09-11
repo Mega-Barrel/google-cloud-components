f9bdb7cd24f62468786fd39afc4459a506645b50
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2025-09-11 15:25:46 +0530

    Removed commented code

--- utils/auth.py
+++ utils/auth.py
@@ -1,44 +1,4 @@
 
-# import os
-# from google.oauth2 import service_account
-
-# class GCPAuth:
-#     """
-#     Base class for authenticating with Google Cloud Platform APIs.
-#     It uses 'credentials.json' file for service account authentication.
-#     """
-#     def __init__(self, credentials_path = 'credentials.json'):
-#         """
-#         Initializes the credentials and project ID.
-        
-#         Args:
-#             credentials_path (str): The path to the credentials.json file.
-#         """
-#         self.__credentials = None
-#         self.__project_id = None
-
-#         # Attempt to load credentials from the specified JSON file
-#         if os.path.exists(credentials_path):
-#             try:
-#                 self.__credentials = service_account.Credentials.from_service_account_file(credentials_path)
-#                 print(f"Successfully authenticated using {credentials_path}")
-#                 # Get the project ID from the credentials
-#                 self.__project_id = self.__credentials.project_id
-#             except Exception as e:
-#                 print(f"Failed to load credentials from {credentials_path}: {e}")
-
-#     def get_credentials(self):
-#         """
-#         Returns the authenticated credentials object.
-#         """
-#         return self.__credentials
-
-#     def get_project_id(self):
-#         """
-#         Returns the project ID associated with the credentials.
-#         """
-#         return self.__project_id
-
 import os
 
 from google.oauth2 import service_account
@@ -86,7 +46,7 @@ class GCPAuth:
             print("The credentials file is invalid. Please check the file content.")
         except DefaultCredentialsError as e:
             print(f"Authentication failed: {e}")
-            print("Failed to get default credentials. Ensure you have authenticated using 'gcloud auth application-default login' or that the credentials file is valid.")
+            print("Failed to get default credentials. Ensure that the credentials file is valid.")
         except Exception as e:
             print(f"An unexpected error occurred during authentication: {e}")
 
@@ -107,8 +67,3 @@ class GCPAuth:
             str: The project ID or None if not available.
         """
         return self.__project_id
-
-if __name__ == "__main__":
-    GCPInstance = GCPAuth()
-    print(GCPInstance.get_credentials())
-    print(GCPInstance.get_project_id())

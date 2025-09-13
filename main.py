58300b7f7eda9f1cefe4fe7fc46498d64346ccb2
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2025-09-13 22:33:04 +0530

    added methods to run code

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 
-from utils.cloud_storage import GCPStorage
+from google_cloud_components.cloud_storage import GCPStorage
 
 if __name__ == "__main__":
     storage_ob = GCPStorage()
     if not storage_ob.get_credentials():
         print("Exiting as authentication failed.")
     else:
-        # BUCKET_NAME = f"{storage_ob.get_project_id()}-test-bucket-example"
         BUCKET_NAME = "udemy-cource"
         storage_ob.list_blobs(bucket_name=BUCKET_NAME)
-        # storage_ob.create_blob(bucket_name=BUCKET_NAME, source_file_name='packsges.txt', destination_blob_name="packsges.txt")
+        # bucket_metadata = storage_ob.get_bucket_metadata(bucket_name=BUCKET_NAME)
+        # print(bucket_metadata)
 
-        # print(storage_ob.get_bucket(bucket_name=BUCKET_NAME))
-        # storage_ob.create_bucket(bucket_name=BUCKET_NAME)
-        # storage_ob.list_buckets()
-        # print(storage_ob.get_bucket(bucket_name=BUCKET_NAME))
+        blob_metadata = storage_ob.get_blob_metadata(bucket_name=BUCKET_NAME, blob_name='csv/2019-04-27.csv')
+        print(blob_metadata)
